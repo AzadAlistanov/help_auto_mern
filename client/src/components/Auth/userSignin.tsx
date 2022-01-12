873f3192f8741d78b0787b38888fae449d2fba16
Nikolay Gertiy <ngertiy@mail.ru>
2022-01-12 17:49:41 +0400

    add half signIn

--- client/src/components/Auth/userSignin.tsx
+++ client/src/components/Auth/userSignin.tsx
@@ -1,8 +1,59 @@
 
-export default function UserSignin() {
+import React, { useState } from 'react'
+
+
+export default function ToSignIn() {
+
+  const [value, setValue] = useState({ name: "", email: "", password: "" })
+
+  function signIn() {
+    console.log(value)
+  }
+
+
   return (
-    <div>
-      UserSignin
-    </div>
+
+    <section className="vh-100 bg-image">
+      <div className="mask d-flex align-items-center h-100 gradient-custom-3">
+        <div className="container h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col-12 col-md-9 col-lg-7 col-xl-6">
+              <div className="card" >
+                <div className="card-body p-5">
+                  <h2 className="text-uppercase text-center mb-5">Create an account</h2>
+
+                  <form >
+
+                    <div className="form-outline mb-4">
+                      <input onChange={(event) => setValue({ ...value, name: event.target.value })} value={value.name} type="name" id="form3Example1cg" className="form-control form-control-lg" />
+                      <label className="form-label" htmlFor="form3Example1cg">Your Name</label>
+                    </div>
+
+                    <div className="form-outline mb-4">
+                      <input onChange={(event) => setValue({ ...value, email: event.target.value })} value={value.email} type="email" id="form3Example3cg" className="form-control form-control-lg" />
+                      <label className="form-label" htmlFor="form3Example3cg">Your Email</label>
+                    </div>
+
+                    <div className="form-outline mb-4">
+                      <input onChange={(event) => setValue({ ...value, password: event.target.value })} value={value.password} type="password" id="form3Example4cg" className="form-control form-control-lg" />
+                      <label className="form-label" htmlFor="form3Example4cg">Password</label>
+                    </div>
+
+                    <div className="d-flex justify-content-center">                
+
+                      <button onClick={signIn} type="button" className="btn btn-info">Register</button>
+                    </div>
+
+
+
+                  </form>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
