84d9f9ff72434d0fce1562adb45574db5cb66190
Azad <alistanov.azad@mail.ru>
2022-01-13 14:09:41 +0300

    router-servicelist

--- backend/app.js
+++ backend/app.js
@@ -26,6 +26,8 @@ const sessionConfig = {
 };
 
 const home = require('./routes/home');
+const serviceList = require('./routes/serviceList');
+
 
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
@@ -34,6 +36,7 @@ app.use(cors({ credentials: true, origin: '*' }));
 
 
 app.use('/', home);
+app.use('/servicelist', serviceList);
 
 
 // app.use(cookieParser());
--- client/src/components/ServiceList/index.tsx
+++ client/src/components/ServiceList/index.tsx
@@ -1,5 +1,17 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { Link } from 'react-router-dom';
 
 export default function ServiceList() {
+
+  const getServices = async () => {
+    axios.get('http://localhost:5000/servicelist')
+    .then((res) => console.log(res.data))}
+  
+  useEffect (() => {
+    getServices()
+  }, [])
+
   return (
     <div>
       ServiceList
