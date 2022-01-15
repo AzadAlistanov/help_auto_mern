1e7896ada676cba7497681cc0653874b5516b5c1
Azad <alistanov.azad@mail.ru>
2022-01-15 18:32:09 +0300

    message1

--- backend/routes/serviceList.js
+++ backend/routes/serviceList.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
 
-const { getServices, addOrder } = require('../controllers/serviceController');
+const { getServices, addOrder, changeStatus } = require('../controllers/serviceController');
 
 
 router.get('/', getServices);
 
 router.post('/neworder', addOrder)
+
+router.get('/order/:orderNumber/:userId/:serviceId', changeStatus);
+
 
 module.exports = router;
